5ca38e369b84e5ceca08c3d05be172c27162ec8d
SaiHormKham-BlissStock <69182517+SaiHormKham-BlissStock@users.noreply.github.com>
2022-02-10 08:07:19 +0630

    update: api end point

--- server/routes/book.route.js
+++ server/routes/book.route.js
@@ -1,27 +1,24 @@
-
 // Express App Setup
 import { Router } from "express";
 const router = Router();
-import BookService  from "../src/service/book.service.js"
+import BookService from "../src/service/book.service.js";
 import { body, param, validationResult } from "express-validator";
-import { mongoIdValidator } from "../src/validator/validator.js"
-
+import { mongoIdValidator } from "../src/validator/validator.js";
 
 const bookService = new BookService();
 
-
- /**
+/**
  * @swagger
  * /books/id/{id}:
  *   get:
  *     summary: Retrieve a single JSONPlaceholder book.
  *     description: Retrieve a single JSONPlaceholder book. Can be used to populate a book profile when prototyping or testing an API.
- *     parameters: 
+ *     parameters:
  *      - in: path
  *        name: id
  *        required: true
  *        description: Mongo ID of the book, a string of 12 bytes or a string of 24 hex characters
- *        schema: 
+ *        schema:
  *         type: string
  *     responses:
  *       200:
@@ -58,42 +55,43 @@ const bookService = new BookService();
  *                       type: integer
  *                       description: the version key that describes the internal revision of a document.
  *                       example: 0
- * 
  *
-*/
-router.get("/id/:id",[
-  param('id').custom(mongoIdValidator)
-], async (req, res) => {
-  const id = req.params.id;
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    console.log(errors);
-    return invalidArgumentResponse(res, errors);
+ *
+ */
+router.get(
+  "/id/:id",
+  [param("id").custom(mongoIdValidator)],
+  async (req, res) => {
+    const id = req.params.id;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      console.log(errors);
+      return invalidArgumentResponse(res, errors);
+    }
+    const result = await bookService.getBookById(id);
+    res.status(result.code).send(result.body);
   }
-  const result = await bookService.getBookById(id);
-  res.status(result.code).send(result.body);
-});
-
+);
 
 router.get("/reading", async (req, res) => {
   const result = await bookService.getAllReadingBook();
   res.status(result.code).send(result.body);
 });
 
-router.get("/finished/:page *?", async (req, res) => {
-  const page = req.params.page;
+router.get("/finished/", async (req, res) => {
+  const page = req.query.page;
   const result = await bookService.getAllFinishedBook(page);
   res.status(result.code).send(result.body);
 });
 
- /**
+/**
  * @swagger
  * /books/reading:
  *   post:
  *     summary: Insert a current Reading book.
  *     description: Insert a single JSONPlaceholder book. Can be used to insert a book profile when prototyping or testing an API.
  *     parameters:
- *      
+ *
  *     responses:
  *       200:
  *         description: A single book.
@@ -113,7 +111,7 @@ router.get("/finished/:page *?", async (req, res) => {
  *                       type: string
  *                       description: The book's name.
  *                       example: Leanne Graham
-*/
+ */
 router.post(
   "/reading",
   [body("name").not().isEmpty(), body("author").not().isEmpty()],
@@ -125,7 +123,7 @@ router.post(
       return invalidArgumentResponse(res, errors);
     }
     const result = await bookService.insertReadingBook(req.body);
-    return res.status(result.code).json(result);
+    return res.status(result.code).json(result.body);
   }
 );
 
@@ -148,8 +146,8 @@ router.patch(
 );
 
 router.delete(
-  "/",
-  [body("id").not().isEmpty(), body("id").custom(mongoIdValidator)],
+  "/id/:id",
+  [param("id").custom(mongoIdValidator)],
   async (req, res) => {
     console.log(req.body);
     // Validate Field
@@ -166,11 +164,9 @@ router.delete(
 );
 
 const invalidArgumentResponse = (res, errors) => {
-  return res
-    .status(400)
-    .json({
-      errors: { message: "Invalid arguments", details: errors.array() },
-    });
+  return res.status(400).json({
+    errors: { message: "Invalid arguments", details: errors.array() },
+  });
 };
 
 export default router;
