b20a39f559e39351058a1adbac23625338fd3dd5
SaiHormKham-BlissStock <69182517+SaiHormKham-BlissStock@users.noreply.github.com>
2022-02-12 10:18:56 +0630

    update: swagger docs

--- server/routes/book.route.js
+++ server/routes/book.route.js
@@ -33,6 +33,13 @@
  *           $ref: "#/components/responses/NotFound" 
  *         '500':
  *           $ref: "#/components/responses/InternalServerError"  
+ *         '400':
+ *           description: "Bad Request"
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 oneOf:
+ *                   - $ref: "#/components/schemas/InvalidArgument" 
  *     delete:
  *       tags:
  *       - "books"
